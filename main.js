084f9f4929d3e5c8c023dab056eb4b5fdd2a9d2e
DarienEstebanVesgaCarvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2025-03-27 12:15:38 -0500

    feat: :sparkles: Create mainMenu
    MainMenu is created

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-let pets = [
+    //📦 Se declara la constante de pets.
+const pets = [
     
 ];
+
+    //📦 Se declara la función del menú principal.
+function mainMenu() {
+    let option;
+    do {
+        option = prompt(
+            "📋 Menú Veterinaria:\n\n" +
+            "1️⃣ Registrar una nueva mascota.\n" +
+            "2️⃣ Listar todas las mascotas.\n" +
+            "3️⃣ Buscar una mascota por nombre.\n" +
+            "4️⃣ Actualizar el estado de salud de una mascota.\n" +
+            "5️⃣ Eliminar una mascota por nombre.\n" +
+            "6️⃣ Salir.\n\n" +
+            "Elige una opción:"
+        );
+
+    } while (option !== "6");
+}
+//🔥 Se inicia el programa.
+mainMenu();
